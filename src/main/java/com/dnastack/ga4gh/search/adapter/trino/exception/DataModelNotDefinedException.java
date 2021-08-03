86b27829c48363eedf0d309f16289aafab525780
Karthika Sobhana <karthika@dnastack.com>
2021-08-03 10:15:04 -0400

    [#178912126]Update folder names to trino.

--- src/main/java/com/dnastack/ga4gh/search/adapter/trino/exception/DataModelNotDefinedException.java
+++
@@ -1,7 +0,0 @@
-package com.dnastack.ga4gh.search.adapter.trino.exception;
-
-public class DataModelNotDefinedException extends RuntimeException {
-    public DataModelNotDefinedException(String message) {
-        super(message);
-    }
-}
