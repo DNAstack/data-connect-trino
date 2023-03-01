31f608d58656d96b361e4ad6c62af72d143ee5fb
Angelo Genovese <102988401+angelo-DNAStack@users.noreply.github.com>
2023-03-01 15:35:52 -0500

    [#184552895] removing query from application logs and audit logs (#19)

--- src/main/java/com/dnastack/ga4gh/dataconnect/adapter/trino/DataConnectRequest.java
+++ src/main/java/com/dnastack/ga4gh/dataconnect/adapter/trino/DataConnectRequest.java
@@ -1,10 +1,12 @@
 package com.dnastack.ga4gh.dataconnect.adapter.trino;
 
+import com.dnastack.audit.util.AuditIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class DataConnectRequest {
+    @AuditIgnore
     @JsonProperty("query")
     private String sqlQuery;
 }
