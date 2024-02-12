ccdab9d76423c44ac4e793dcff1d2b5a08c4cf21
jstromsky <jakub@dnastack.com>
2024-02-12 12:08:18 +0100

    [#186967106] Add code analysis and fix deprecated annotation

--- src/main/java/com/dnastack/ga4gh/dataconnect/adapter/security/GlobalMethodSecurityConfig.java
+++ src/main/java/com/dnastack/ga4gh/dataconnect/adapter/security/GlobalMethodSecurityConfig.java
@@ -3,9 +3,10 @@ package com.dnastack.ga4gh.dataconnect.adapter.security;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
 @ConditionalOnExpression("${app.auth.global-method-security.enabled:true}")
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class GlobalMethodSecurityConfig {
 }
