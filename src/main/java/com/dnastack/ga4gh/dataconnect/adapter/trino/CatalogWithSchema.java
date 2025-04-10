4747c3a749be59b38b5b9943b7e5f5d7c78277cb
Bazlur Rahman <anmbazlur@dnastack.com>
2025-04-10 16:37:00 -0400

    [CU-86b4frdzu] Simplify CatalogWithSchema by removing custom comparison logic

--- src/main/java/com/dnastack/ga4gh/dataconnect/adapter/trino/CatalogWithSchema.java
+++ src/main/java/com/dnastack/ga4gh/dataconnect/adapter/trino/CatalogWithSchema.java
@@ -1,13 +1,3 @@
 package com.dnastack.ga4gh.dataconnect.adapter.trino;
 
-import java.util.Comparator;
-
-public record CatalogWithSchema(String catalogName, String schema) implements Comparable<CatalogWithSchema> {
-    @Override
-    public int compareTo(CatalogWithSchema o) {
-
-        return Comparator.comparing(CatalogWithSchema::catalogName)
-                .thenComparing(CatalogWithSchema::schema)
-                .compare(this, o);
-    }
-}
+public record CatalogWithSchema(String catalogName, String schema) { }
